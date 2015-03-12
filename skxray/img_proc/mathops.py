30b01e225afac2672aaab3bc8c14eace7acddbbb
Gabriel Iltis <giltis@bnl.gov>
2015-03-12 14:01:08 -0400

    DEV: Initial commit after breaking down the img proc mods.
    This is the initial commit in the new image arithmetic branch, which has
    been created in order to simplify addition and merging of the compiled
    image processing tools to skxray. This branch will focus exclusively on
    image arithmetic and logical operations, tests of these functions,
    demonstration of function use, as well as helper functions to a) keep
    track of, and present data set attributes, and b) validate and adjust
    attributes such as array dimensions if input arrays mismatch. User
    friendly errors are also being integrated to aid the user in correcting
    errors such as differences in dimensionality, or dtype.

---
+++ skxray/img_proc/mathops.py
@@ -0,0 +1,14 @@
+# Module for the BNL image processing project
+# Developed at the NSLS-II, Brookhaven National Laboratory
+# Developed by Gabriel Iltis, Oct. 2013
+"""
+# This module contains the tools and functions required for basic image
+arithmetic and logical operations. While the tools included are able to be
+executed at the python interpreter prompt, or as part of a image processing
+python script, the module has been designed to facilitate image arithmetic
+and logical operations on image data sets in the VisTrails workflow manager.
+"""
+
+import numpy as np
+
+import logging
