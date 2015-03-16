4a014421a40fa9cd03859fb78e09e25a6dde3e41
Gabriel Iltis <giltis@bnl.gov>
2015-03-16 11:31:44 -0400

    DEV: Added synthetic data generators to img_proc test file

--- skxray/tests/test_img_proc.py
+++ skxray/tests/test_img_proc.py
@@ -15,3 +15,21 @@ import six
 from nose.tools import eq_
 from skxray.img_proc import mathops
 from numpy.testing import assert_equal
+
+
+test_array_1 = np.zeros((30,30,30), dtype=int)
+test_array_1[0:15, 0:15, 0:15] = 1
+test_array_2 = np.zeros((50, 70, 50), dtype=int)
+test_array_2[25:50, 25:50, 25:50] = 87
+test_array_3 = np.zeros((10,10,10), dtype=float)
+test_array_4 = np.zeros((100,100,100), dtype=float)
+test_array_5 = np.zeros((100,100), dtype=int)
+test_array_5[25:75, 25:75] = 254
+
+test_1D_array_1 = np.zeros((100), dtype=int)
+test_1D_array_2 = np.zeros((10), dtype=int)
+test_1D_array_3 = np.zeros((100), dtype=float)
+
+test_constant_1 = 5
+test_constant_2 = 2.0
+test_constant_3 = 1
